cdec86cd9efff695006f6fdf65f25f2b3731fb70
Riley Kilgore <riley-kilgore@github.com>
2020-03-05 14:54:42 -0800

    Added documentation to Store.py

--- Store.py
+++ Store.py
@@ -1,9 +1,14 @@
+"""
+The Store is used to represent a the physical store in the model.
+The Store orchestrates the operations of the other classes.
+"""
 from CustomerAgent import CustomerAgent
 from EmployeeCheckOutAgent import EmployeeCheckOutAgent
 from SelfCheckOutAgent import SelfCheckOutAgent
 from random import random
 
 class Store(object):
+    # Constructor
     def __init__(self, n):
         self.global_time = 0
         self.store = []
