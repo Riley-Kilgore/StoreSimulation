f341d96f47104936c329bfed557bbff2567f2ce9
BeeTeePhoneHome <60676566+BeeTeePhoneHome@users.noreply.github.com>
2020-03-05 07:02:30 -0800

    Update CustomerAgent.py
    Updated cart size initialization based on statistics

--- CustomerAgent.py
+++ CustomerAgent.py
@@ -1,6 +1,16 @@
+import random
+
 class CustomerAgent(object):
     def __init__(self):
-        self.items_per_step = 1
-        self.time_elasped = 0
+        self.secPerItem = 5
+        self.timeElapsed= 0
+        self.cartSize = 0
+
+        pdfNum = random.randint(0,250)
+        if(pdfNum <= 50) cartSize = random.randint(1,2)
+        elif(pdfNum <= 110) cartSize = random.randint(3,10)
+        elif(pdfNum <= 184) cartSize = random.randint(11,20)
+        else cartSize = random.randint(21,60)
     
     def choose_checkout(self, lines):
+        return lane = random.randint(0, len(store))
