5b33f8f10f05143e7689ff97bea4b162f0231787
Riley Kilgore <riley-kilgore@github.com>
2020-03-05 13:08:39 -0800

    Forgot to commit yesterday at end of meeting.

--- EmployeeCheckOutAgent.py
+++ EmployeeCheckOutAgent.py
@@ -5,7 +5,10 @@ class EmployeeCheckOutAgent(object):
         self.items_per_step = 5
         self.customers = queue.Queue()
         self.current_customer = None
+        self.
     
     def process(self):
+        if self.customers.empty() and not self.current_customer:
+            break
         self.current_customer = self.current_customer if self.current_customer != None else self.customers.get()
         self.current_customer.process_with(self.items_per_step)
--- SelfCheckOutAgent.py
+++ SelfCheckOutAgent.py
@@ -1,3 +1,15 @@
+import queue
+from constants import *
+
 class SelfCheckOutAgent(object):
     def __init__(self):
-        pass
+        self.seconds_per_item = 5
+        self.customers = queue.Queue()
+        self.current_customer = None
+        self.
+    
+    def process(self):
+        if self.customers.empty() and not self.current_customer:
+            break
+        self.current_customer = self.current_customer if self.current_customer != None else self.customers.get()
+        self.current_customer.process_with(self.items_per_step)
