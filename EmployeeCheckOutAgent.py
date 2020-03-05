a0e5723cad66486dd0753c1c4c7eac965ce84fc4
BeeTeePhoneHome <60676566+BeeTeePhoneHome@users.noreply.github.com>
2020-03-05 06:01:52 -0800

    Update EmployeeCheckOutAgent.py

--- EmployeeCheckOutAgent.py
+++ EmployeeCheckOutAgent.py
@@ -2,10 +2,14 @@ import queue
 
 class EmployeeCheckOutAgent(object):
     def __init__(self):
-        self.items_per_step = 5
+        self.secPerItem= 2
         self.customers = queue.Queue()
         self.current_customer = None
+        eventClock = 0
     
     def process(self):
         self.current_customer = self.current_customer if self.current_customer != None else self.customers.get()
-        self.current_customer.process_with(self.items_per_step)
+        self.current_customer.process_with(self.secPerItem)
+
+    def tick(self):
+        self.eventClock = self.eventClock + 1
