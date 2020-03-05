58579ca492af173dc9cc87738063e171fbef88b2
Riley Kilgore <riley-kilgore@github.com>
2020-03-05 13:46:42 -0800

    Made changes to processing logic, getting closer to working version. Docs are next.

--- EmployeeCheckOutAgent.py
+++ EmployeeCheckOutAgent.py
@@ -5,17 +5,20 @@ class EmployeeCheckOutAgent(object):
         self.secPerItem= 2
         self.customers = queue.Queue()
         self.currentCustomer = None
-        eventClock = 0
+        self.eventClock = 0
     
     def process(self):
         if self.customers.empty() and not self.currentCustomer:
             return None
+        if self.currentCustomer is None:
+            self.eventClock = 0
         self.currentCustomer = self.currentCustomer if self.currentCustomer != None else self.customers.get()
-        self.currentCustomer = self.currentCustomer.process_with(self.secPerItem)
+        self.currentCustomer = self.currentCustomer.process_with(self.eventClock, self.secPerItem)
+        self.tick()
         return self.currentCustomer
 
     def tick(self):
-        self.eventClock = self.eventClock + 1
+        self.eventClock += 1
 
     def addToLine(self, customer):
         self.customers.put(customer)
--- SelfCheckOutAgent.py
+++ SelfCheckOutAgent.py
@@ -6,10 +6,19 @@ class SelfCheckOutAgent(object):
         self.secPerItem = 5
         self.customers = queue.Queue()
         self.currentCustomer = None
+        self.eventClock = 0
     
     def process(self):
         if self.customers.empty() and not self.currentCustomer:
             return None
+        if self.currentCustomer is None:
+            self.eventClock = 0
         self.currentCustomer = self.currentCustomer if self.currentCustomer != None else self.customers.get()
-        self.currentCustomer = self.currentCustomer.process_with(self.secPerItem)
+        self.currentCustomer = self.currentCustomer.process_with(self.eventClock, self.secPerItem)
         return self.currentCustomer
+    
+    def tick(self):
+        self.eventClock = self.eventClock + 1
+
+    def addToLine(self, customer):
+        self.customers.put(customer)
