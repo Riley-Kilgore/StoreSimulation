67f5b18692a3a42750561d8ab3463875d216bc26
Victor Suciu <victor@victors-mbp-4.lan>
2020-03-10 15:34:34 -0700

    added customer choosing logic

--- checkoutLogic.py
+++ checkoutLogic.py
@@ -1,14 +1,35 @@
 import random
 import Store
 import CustomerAgent
+from constants import *
 
 
-def choose_checkout(numOfLines):
-    """
-    Returns the index in the line that the customer should go to.
-    This is currently stochastic and based upon a uniform distribution.
-    """
-    return random.randint(0, numOfLines)
+def choose_checkout(self, lines):
+    curr_choice = 0
+    best_stats = lines[0].get_decision_factors()
+    index = 0
+
+    for line in lines:
+        stats = line.get_decision_factors()
+
+        # if carts in the new line have visibly fewer items than the current best line
+        if stats[0] + (VISIBLE_ITEM_FRAC * stats[1]) <= best_stats[0]:
+            curr_choice = index
+            best_stats = stats
+
+        # if the new line is shorter than the current best line
+        elif stats[1] < best_stats[1]:
+            curr_choice = index
+            best_stats = stats
+
+        # if the new line is a self checkout and the current best is an employee
+        elif stats[2] == 'self' and best_stats[2] == 'employee':   
+            curr_choice = index
+            best_stats = stats
+
+            index += 1
+
+    return lines[curr_choice]
 
 
 def move_towards_location(x1, y1, x2, y2):
