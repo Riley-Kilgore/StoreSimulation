73b63d7c3fa15513f923e0b4fb173fb157af31b0
Riley Kilgore <riley-kilgore@github.com>
2020-03-12 13:49:54 -0700

    Deleted redundant code.

--- Driver.py
+++ Driver.py
@@ -108,11 +108,6 @@ if __name__ == "__main__":
     testingSuite += 1
     print("Finished round ", testingSuite, "of 5")
 
-
-    list(itertools.product([5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
-                              29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40],[.1, .2, .3, .4, .5, .6, .7, .8, .9, 1]))
-
-
     with open("employee_sp_chance_self.csv", "w") as f:
         writer = csv.writer(f)
         writer.writerows(zip(list(itertools.product([5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
