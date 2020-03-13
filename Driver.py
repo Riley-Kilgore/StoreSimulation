cc104c705d89d3218887381424f68d412d410177
Riley Kilgore <riley-kilgore@github.com>
2020-03-12 18:40:53 -0700

    Added settings in driver, and added documentation.

--- Driver.py
+++ Driver.py
@@ -1,3 +1,10 @@
+# The Driver Module
+# The Driver Module is a single function module that is used
+#   to tie together the model for our simulation.
+# In order to use the program, the user must tweak the settings
+#   below. All program functionality is handled through the settings
+#   here.
+
 from Store import Store, SelfCheckOutAgent, EmployeeCheckOutAgent
 import matplotlib.pyplot as plt
 import time
@@ -6,24 +13,50 @@ from matplotlib.animation import ArtistAnimation
 import csv
 import itertools
 
+# ****************** Settings *********************************
+NUM_REGISTERS = 15
+ITEM_M_E = 25
+ITEM_M_S = 15
+CHANCE_SELF_CHECK = .5
+SPAWN_CHANCE = 1
 
-def get_store_metrics(numRegisters, secPerItemE, secPerItemS,
+REG_LIST = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30]
+ITEM_E_LIST = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+               19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+               32, 33, 34, 35, 36, 37, 38, 39, 40]
+ITEM_S_LIST = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+SELF_CHECK_SPAWN = [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
+CUSTOMER_SPAWN = [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
+
+
+
+def get_store_metrics(numRegisters, itemPerMinE, itemPerMinS,
                       chanceSelfCheckout, customerSpawnRate):
-    store = Store(numRegisters, secPerItemE, secPerItemS, chanceSelfCheckout, customerSpawnRate)
-    simTime = 86400
-    # grids_over_time = []
-
-    # fig = plt.figure()
-    # ims = []
-
+    """
+    get_store_metrics takes in 5 parameters and returns metrics
+    on how the business has done given the parameters.
+    :param: numRegisters: The number of registers in the store.
+    :param: itemPerMinE: The average number of items processed by
+      employees.
+    :param: itemPerMinS: The average number of items processed by
+      customers in self-checkout.
+    :param: checkSelfCheckout: The chance that given a new CheckOut location,
+      the CheckOut location is a self-checkout kiosk.
+    :param: customerSpawnRate: The chance on a customer-spawn-tick that the
+      customer actually spawns, alternatively no customer will be generated on that
+      tick.
+    :return: Average Wait, Total Customers, Total Customers Processed,
+      Num of Employee Registers, Num of Self-Checkout Kiosks.
+    """
+    # Get all store
+    store = Store(numRegisters, itemPerMinE, itemPerMinS, chanceSelfCheckout, customerSpawnRate) simTime = 86400
+    
     start = time.process_time()
 
     print("Starting simulation suite")
 
     for t in range(simTime):
-        # ims.append([plt.imshow(store.grid)])
         store.run_step()
-    # ims.append([plt.imshow(store.grid)])
 
     totalProcessed = 0
 
@@ -32,8 +65,6 @@ def get_store_metrics(numRegisters, secPerItemE, secPerItemS,
         totalProcessed += register.customersProcessed
 
     totalCustomers = store.totalCustomers
-    # for i in range(24):
-    # totalCustomers += store.simCustomers[i]
 
     totalWaitTime = 0
     self_kiosks = 0
@@ -56,80 +87,74 @@ def get_store_metrics(numRegisters, secPerItemE, secPerItemS,
     # print("Time per person: ", round(simTime / (totalProcessed / numRegisters), 2), "seconds per customer")
     return totalWaitTime / (60 * store.temp_total), store.temp_total, totalProcessed, emp_regs, self_kiosks
 
-    # animation = ArtistAnimation(fig, ims, interval=10, blit=True)
-
-    # plt.show()
-
 
 if __name__ == "__main__":
-    numRegisters = 15
-    secPerItemE = 25
-    secPerItemS = 15
-    chanceSelfCheckout = .5
-    customerSpawnRate = 1
-    # get_store_metrics(numRegisters, secPerItemE, secPerItemS, chanceSelfCheckout, customerSpawnRate)
+    # DEFAULT VALUES
+    numRegisters = NUM_REGISTERS
+    itemPerMinE = ITEM_M_E
+    itemPerMinS = ITEM_M_S
+    chanceSelfCheckout = CHANCE_SELF_CHECK
+    customerSpawnRate = SPAWN_CHANCE
+    # get_store_metrics(numRegisters, itemPerMinE, itemPerMinS, chanceSelfCheckout, customerSpawnRate)
 
     # logging how long the simulation suite takes
     startTime = datetime.datetime.now().replace(microsecond=0)
 
     testingSuite = 0
     print("Finished round ", testingSuite, "of 5")
-    register_list = [get_store_metrics(reg_val, secPerItemE, secPerItemS, chanceSelfCheckout, customerSpawnRate)
-                     for reg_val in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30]]
+    register_list = [get_store_metrics(reg_val, itemPerMinE, itemPerMinS, chanceSelfCheckout, customerSpawnRate)
+                     for reg_val in REG_LIST]
     testingSuite += 1
     print("Finished round ", testingSuite, "of 5")
 
     employee_speed_list = [
-        get_store_metrics(numRegisters, item_speed, secPerItemS, chanceSelfCheckout, customerSpawnRate)
-        for item_speed in [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
-                           29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]]
+        get_store_metrics(numRegisters, item_speed, itemPerMinS, chanceSelfCheckout, customerSpawnRate)
+        for item_speed in ITEM_E_LIST]
     testingSuite += 1
     print("Finished round ", testingSuite, "of 5")
 
     employee_speed_self_chance_list = [
-        get_store_metrics(numRegisters, item_speed, secPerItemS, chance, customerSpawnRate)\
-            for item_speed in [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
-                           29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40] for chance in\
-                           [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
+        get_store_metrics(numRegisters, item_speed, itemPerMinS, chance, customerSpawnRate)\
+            for item_speed in ITEM_E_LIST for chance in SELF_CHECK_SPAWN
     ]
 
-    self_speed_list = [get_store_metrics(numRegisters, secPerItemE, item_speed, chanceSelfCheckout, customerSpawnRate)
-                       for item_speed in [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]]
+    self_speed_list = [get_store_metrics(numRegisters, itemPerMinE, item_speed, chanceSelfCheckout, customerSpawnRate)
+                       for item_speed in ITEM_S_LIST]
     testingSuite += 1
     print("Finished round ", testingSuite, "of 5")
 
-    # chance_self_list = [get_store_metrics(numRegisters, secPerItemE, secPerItemS, indiv_chance, customerSpawnRate)
-    #                     for indiv_chance in [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1]]
-    # testingSuite += 1
-    # print("Finished round ", testingSuite, "of 5")
-
-    spawn_rate_list = [get_store_metrics(numRegisters, secPerItemE, secPerItemS, chanceSelfCheckout, indiv_chance)
-                       for indiv_chance in [.1, .2, .3, .4, .5]]
+    chance_self_list = [get_store_metrics(numRegisters, itemPerMinE, itemPerMinS, indiv_chance, customerSpawnRate)
+                         for indiv_chance in SELF_CHECK_SPAWN]
     testingSuite += 1
     print("Finished round ", testingSuite, "of 5")
 
-    with open("employee_sp_chance_self.csv", "w") as f:
-        writer = csv.writer(f)
-        writer.writerows(zip(list(itertools.product([5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
-                              29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40],[.1, .2, .3, .4, .5, .6, .7, .8, .9, 1])), employee_speed_self_chance_list))
+    spawn_rate_list = [get_store_metrics(numRegisters, itemPerMinE, itemPerMinS, chanceSelfCheckout, indiv_chance)
+                       for indiv_chance in CUSTOMER_SPAWN]
+    testingSuite += 1
+    print("Finished round ", testingSuite, "of 5")
+
+    # This block runs too many simulations at once.
+    # with open("employee_sp_chance_self.csv", "w") as f:
+    #     writer = csv.writer(f)
+    #     writer.writerows(zip(list(itertools.product([5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
+    #                           29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40],[.1, .2, .3, .4, .5, .6, .7, .8, .9, 1])), employee_speed_self_chance_list))
 
     with open("register_data_expanded.csv", "w") as f:
         writer = csv.writer(f)
-        writer.writerows(zip([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30], register_list))
+        writer.writerows(zip(REG_LIST, register_list))
     with open("employee_speed_data_expanded.csv", "w") as f:
         writer = csv.writer(f)
-        writer.writerows(zip([5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
-                              29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40], employee_speed_list))
+        writer.writerows(zip(ITEM_E_LIST, employee_speed_list))
     with open("self_speed_data_expanded.csv", "w") as f:
         writer = csv.writer(f)
-        writer.writerows(zip([6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25],
+        writer.writerows(zip(ITEM_S_LIST,
                              self_speed_list))
-    # with open("self_checkout_chance_data_expanded.csv", "w") as f:
-    #     writer = csv.writer(f)
-    #     writer.writerows(zip([.1, .2, .3, .4, .5], chance_self_list))
+    with open("self_checkout_chance_data_expanded.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(zip(SELF_CHECK_SPAWN, chance_self_list))
     with open("customer_spawn_rate_data_expanded", "w") as f:
         writer = csv.writer(f)
-        writer.writerows(zip([.1, .2, .3, .4, .5, .6, .7, .8, .9, 1], spawn_rate_list))
+        writer.writerows(zip(CUSTOMER_SPAWN, spawn_rate_list))
 
     endTime = datetime.datetime.now().replace(microsecond=0)
     print("The simulation suite took ", endTime - startTime, "to run")
